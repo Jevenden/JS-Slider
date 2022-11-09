2d0775903221148f8ea4d8a837f3ce234ae81ff5
Jevenden <102879070+Jevenden@users.noreply.github.com>
2022-11-09 14:45:10 -0500

    completed button and button color functionality

--- slider.js
+++ slider.js
@@ -2,9 +2,34 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const slider = document.querySelector(".slider");
 const images = document.querySelectorAll(".image");
+const bottom = document.querySelector(".bottom");
 
 let slideNumber = 1;
 const length = images.length;
+
+for (let i = 0; i < length; i++) {
+  const dot = document.createElement("div");
+  dot.className = "dot";
+  bottom.appendChild(dot);
+}
+
+const dots = document.querySelectorAll(".dot");
+dots[0].style.backgroundColor = "white";
+
+const resetBgColor = () => {
+  dots.forEach((dot) => {
+    dot.style.backgroundColor = "transparent";
+  });
+};
+
+dots.forEach((dot, index) => {
+  dot.addEventListener("click", () => {
+    resetBgColor();
+    slider.style.transform = `translateX(-${index * 800}px)`;
+    slideNumber = index + 1;
+    dot.style.backgroundColor = "white";
+  });
+});
 
 const nextSlide = () => {
   slider.style.transform = `translateX(-${slideNumber * 800}px)`;
@@ -28,8 +53,12 @@ const getLastSlide = () => {
 
 right.addEventListener("click", () => {
   slideNumber < length ? nextSlide() : getFirstSlide();
+  resetBgColor();
+  dots[slideNumber - 1].style.backgroundColor = "white";
 });
 
 left.addEventListener("click", () => {
   slideNumber > 1 ? prevSlide() : getLastSlide();
+  resetBgColor();
+  dots[slideNumber - 1].style.backgroundColor = "white";
 });
